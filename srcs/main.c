b9230b62a8f3ebd86d282502461f8051daeaf909
eisikogl <isikoglu99@gmail.com>
2022-08-14 19:28:21 +0300

    minishell start

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,31 @@
 /*   By: eisikogl <eisikogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 00:06:20 by eisikogl          #+#    #+#             */
-/*   Updated: 2022/08/13 00:06:52 by eisikogl         ###   ########.fr       */
+/*   Updated: 2022/08/14 19:26:52 by eisikogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include <stdio.h>
+#include <unistd.h>
+#include "../libs/libft/libft.h"
+
+int main(int argc, char **argv, char **envp)
 {
+    // char line[9999];
+    // int i = 0;
+    // while (envp[i])
+    // {
+    //     if (!ft_strncmp(envp[i], "PATH=",  5))
+    //     {
+    //         printf("%s: \n", envp[i] + 5);
+    //         line = envp[i] + 5;
+    //     }
+
+    //     i++;
+    //     /* code */
+    // }
     
+    char cmd[] = "/bin/ls";
+    char *argve[] = {"ls", "-l", NULL};
+    execve(cmd, argve, NULL);
 }
